94ac8cad180ed5737d4847762ff2af9601680779
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-11-28 05:10:00 -0800

    u

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-number = 0
-while number <= 89:
-    if number % 10 == 0:
-        number += 1 + number // 10
-    print("{:02d}".format(number), end='\n' if number == 89 else ", ")
-    number += 1
+for num in range(00, 100):
+    print("{:02d}".format(num), end='\n' if num == 99 else ", ")
