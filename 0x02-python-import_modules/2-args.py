fd9200cfd1247da5b7eb23a91b399e1fb66726ec
Ahmed Basher Abdalla <basherahmed362@gmail.com>
2023-11-30 09:24:20 -0800

    u

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,13 @@ if __name__ == "__main__":
     import sys
     args = sys.argv[1:]
     arg_count = len(args)
-    print("{} arguments:".format(arg_count))
-    for arg in args:
-        print("{}: {}".format(args.index(arg) + 1, arg))
+    if arg_count == 0:
+        print("0 arguments.")
+    elif arg_count == 1:
+        print("1 argument:")
+        for arg in args:
+            print("{}: {}".format(args.index(arg) + 1, arg))
+    else:
+        print("{} arguments:".format(arg_count))
+        for arg in args:
+            print("{}: {}".format(args.index(arg) + 1, arg))
